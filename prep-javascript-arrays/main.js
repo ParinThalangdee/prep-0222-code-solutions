4db5c91772c096452439baccdb3b7f28946a3e34
Parin Thalangdee <lotteman10@gmail.com>
2022-03-01 14:06:06 -0800

    Update main.js

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -6,5 +6,6 @@ var thirdElement = languages[2];
 console.log('The third element of the array is:', thirdElement);
 var length = languages.length;
 console.log('The length of the array is:', length);
-var lastElement = languages[-1];
-console.log('The last element of the array is:', lastElement);
+var lastElement = length - 1;
+var lastLanguage = languages[lastElement];
+console.log('The last element of the array is:', lastLanguage);
